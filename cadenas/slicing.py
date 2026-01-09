5ced75287beacf97267f50c3c69f0b12a8815f32
JorgeYael99 <jorg.yael99@gmail.com>
2026-01-09 13:02:45 -0600

    slicing desde linux

---
+++ cadenas/slicing.py
@@ -0,0 +1,18 @@
+# slicing
+texto = "programacion"
+print(texto)
+# muestra los caracteres del 0 al 3
+prim_tres = texto[:4]
+print(prim_tres)
+#muestra en pantalla desde los char -11 al -6
+print(texto[-12:-5])
+#salto de 3 en 3
+print(texto[5::3])
+#todo el texto al reves
+print(texto[::-1])
+
+print(f"""
+======USANDO SLICIN======
+Los primeros 4 caracteres de la palabra {texto} son: {texto[0:4]}
+
+La palabra {texto} alreves se escribe asi: {texto[::-1]}""")
