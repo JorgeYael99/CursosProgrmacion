d4da3517775e2a8aed88e3bfddf615f8ceb0b3bb
JorgeYael99 <jorg.yael99@gmail.com>
2026-02-27 17:00:47 -0600

    ejercicio 2

--- JavaScript/variables/excerciseDos.js
+++ JavaScript/variables/excerciseDos.js
@@ -1,6 +1,19 @@
-// 
-let nombre= "Jorge";
-let edad=27;
-let pais="Mexico";
+// Inicializamos nombres
+const name1 = "Maxwell Wright";
+const name2 = "Raja Villareal";
+const name3 = "Helen Richards";
+// se mandan a llamar numeros telefónicos
+let phone1 = "(0191) 719 6495";
+let phone2="0866 398 2895";
+let phone3="0800 1111";
 
-console.log("usuario. Nombre:",nombre,"edad:",edad,"pais:",pais);
+// Se inicializan Emails
+let email1="Curabitur.egestas.nunc@nonummyac.co.uk";
+let email2="posuere.vulputate@sed.com";
+let email3="libero@convallis.edu";
+
+// se manda a pantalla la lista con los valores respectivos
+console.log("       Name                Phone               Email");
+console.log(name1,"     ",phone1,"      ",email1)
+console.log(name2,"     ",phone2,"        ",email2)
+console.log(name3,"     ",phone3,"            ",email3)
